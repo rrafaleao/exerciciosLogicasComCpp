4f53b9bf0dbf5be5a2ee54516102cf2fc7bda824
rrafaleao <162385496+rrafaleao@users.noreply.github.com>
2024-04-04 20:32:04 +0000

    ExerciciosLogica

--- exercicio15.cpp
+++ exercicio15.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 int main( ){
-    int num1, num2;
+    int a, b;
     printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
+    scanf("%d", &a);
     printf("digite o segundo numero: ");
-    scanf("%d", &num2);
-    
+    scanf("%d", &b);
+    if(a > b){
+        printf("o primeiro número é maior que o segundo \n");
+    }
+    else{
+        printf("o segundo número é maior que o primeiro\n ");
+    }
+    if(a == b){
+        printf("Os dois números são iguais\n");
+    }
+return 0;
 }
